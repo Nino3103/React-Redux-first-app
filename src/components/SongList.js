ef55c1dbfecb8c9827f3379a04c13280105b765c
Nino3103 <jeangilardo@gmail.com>
2019-12-11 19:03:30 +0100

    song list displayed and css formating

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class SongList extends Component {
-  render(){
-    return <div>SongList</div>
+  renderList(){
+
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+
+          <div className = "right floated content">
+            <button className="ui button success">
+              Select
+            </button>
+          </div>
+
+          <div className="content"> {song.title}</div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return <div className = "ui divided list"> {this.renderList()} </div>
   }
 }
 
-export default SongList
+// GET DATA FROM REDUX STORE INTO A COMPONENT 
+// by convention mapStateToProps but any name can works
+// the flow : 1) import { connect } from 'react-redux
+// 2) export default connect(mapStateToProps)(component)
+// 3) define mapStateToProps(state)
+
+const mapStateToProps = (state) => {
+  // the state is stored !
+  // mapStateToProps is always taking first argument of state
+  // and returning an object showing up props inside a component 
+  return { songs: state.songs }
+}
+
+export default connect(mapStateToProps)(SongList)
